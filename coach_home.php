8c4358eacd491c646f0e519a90a4e04fcd4dca7a
SYARAFANA BALQIS BINTI HADI MUDIN <syarafanabalqiss@gmail.com>
2026-06-05 22:04:31 +0800

    Coach module (#1)
    * Remove history and suggestions from coach module
    
    * delete nav

--- coach_home.php
+++ coach_home.php
@@ -7,17 +7,6 @@
 
 <h1>Coach Home</h1>
 
-<nav>
-    <a href="coach_home.php">Home</a> |
-    <a href="coach_profile.php">Profile</a> |
-    <a href="coach_clients.php">Clients</a> |
-    <a href="coach_evaluation.php">Evaluation</a> |
-    <a href="coach_history.php">History</a> |
-    <a href="coach_suggestions.php">Suggestions</a>
-</nav>
-
-<hr>
-
 <h2>Dashboard</h2>
 
 <p>Welcome Coach!</p>
