ae4c1ab594229b27bb4649f7a6c2e5794baf42d6
syarafanabalqiss <syarafanabalqiss@gmail.com>
2026-06-05 22:39:47 +0800

    delete all nav in coach file

--- coach_evaluation.php
+++ coach_evaluation.php
@@ -7,16 +7,8 @@
 
 <h1>Coach Evaluation</h1>
 
-<nav>
-    <a href="coach_home.php">Home</a> |
-    <a href="coach_profile.php">Profile</a> |
-    <a href="coach_clients.php">Clients</a> |
-    <a href="coach_evaluation.php">Evaluation</a> |
-    <a href="coach_history.php">History</a> |
-    <a href="coach_suggestions.php">Suggestions</a>
-</nav>
 
-<hr>
+
 
 <h2>Client Evaluation</h2>
 
