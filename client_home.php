3276771e0b03465f8c6b26188d953f67046fe416
adlina1724 <wnnradlina@gmail.com>
2026-06-06 10:47:14 +0800

    buang fail client lama

--- client_home.php
+++
@@ -1,8 +0,0 @@
-<?php include('navbar.php'); ?>
-
-<h1>Healthy Lifestyle Management System</h1>
-
-<p>
-Welcome to Healthy Lifestyle Management System.
-Track your calories, meals and exercises.
-</p>
