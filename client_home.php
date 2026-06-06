ded24e05f6fcf147f623be2f8afac5a11c6bbef3
adlina1724 <wnnradlina@gmail.com>
2026-06-06 10:16:43 +0800

    ni home client

---
+++ client_home.php
@@ -0,0 +1,8 @@
+<?php include('navbar.php'); ?>
+
+<h1>Healthy Lifestyle Management System</h1>
+
+<p>
+Welcome to Healthy Lifestyle Management System.
+Track your calories, meals and exercises.
+</p>
