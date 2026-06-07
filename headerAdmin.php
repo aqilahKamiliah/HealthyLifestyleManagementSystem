84d9d7e2a2cb4dce42791c57fe55dbcf5501a0e5
adlina1724 <wnnradlina@gmail.com>
2026-06-07 11:36:54 +0800

    push

---
+++ headerAdmin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Healthy LifeStyle Management System</title>
+    <link rel="stylesheet" type="text/css" href="style1.css">
+</head>
+<body>
+    <header>
+        <div class="headTittle">
+        <?php
+        $sytemName= "Healthy LifeStyle Management System";
+        echo "<h1 class='center'>".$sytemName."</h1>";
+        ?>
+        </div>
+
+        <nav>
+        <a href = "homeAdmin.php"> Home </a > 
+        <a href = "listClient.php"> Client </a > 
+        <a href = "listCoach.php"> Coach </a > 
+        <a href = "listFood"> Food </a > 
+        <a href = "logOut.php"> Log Out </a > 
+        </nav>
+    </header>
+    
+</body>
+</html>
