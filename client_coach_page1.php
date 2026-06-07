176e37a350031e6da163d9ed818b9ab579ed1d80
Ainnnizam <ainnizam06@gmail.com>
2026-06-07 16:26:04 +0800

    completed index, client_history and client_coach UIpages and styling

---
+++ client_coach_page1.php
@@ -0,0 +1,95 @@
+<?php include 'headerClient.php'; ?>
+
+<h2 class="coach-title">Choose a coach that fits your goals</h2>
+<div class="coach-container">
+    
+    <div class="coach-card">
+        <div class="avatar"></div>
+        <h3>Sarah</h3>
+        
+        <p>
+            Weight Management,
+            Meal Planning,
+            Calorie Tracking
+        </p>
+
+        <a href="client_coach_page2.php?coach=Sarah">
+            <button>Choose Coach</button>
+        </a>
+    </div>
+    <div class="coach-card">
+        <div class="avatar"></div>
+        <h3>Maria</h3>
+        
+        <p>
+            Stress Management,
+            Flexibility & Mobility,
+            Mindfulness & Meditation
+        </p>
+
+        <a href="client_coach_page2.php?coach=Maria">
+            <button>Choose Coach</button>
+        </a>        
+    </div>
+
+     <div class="coach-card">
+        <div class="avatar"></div>
+        <h3>Aaron</h3>
+        
+        <p>
+            Muscle Building & Toning,
+            Resistance Trainning,
+            Athletic Performance
+        </p>
+
+        <a href="client_coach_page2.php?coach=Aaron">
+            <button>Choose Coach</button>
+        </a>
+    </div>
+    <div class="coach-card">
+        <div class="avatar"></div>
+        <h3>Faeeq</h3>
+        
+        <p>
+            Running & Cycling prep,
+            Cardiovascular health,
+            Stamina Building.
+        </p>
+
+        <a href="client_coach_page2.php?coach=Faeeq">
+            <button>Choose Coach</button>
+        </a>
+    </div>
+    
+    <div class="coach-card">
+        <div class="avatar"></div>
+        <h3>Jack</h3>
+        
+        <p>
+            Rapid Fat Loss,
+            Lean Muscle Gain,
+            Metabolic Optimization
+        </p>
+
+        <a href="client_coach_page2.php?coach=Jack">
+            <button>Choose Coach</button>
+        </a>
+    </div>
+
+    <div class="coach-card">
+        <div class="avatar"></div>
+        <h3>Sofee</h3>
+        
+        <p>
+            Healthy Weight Loss,
+            Hormonal Friendly Dieting,
+            Body Recomposition
+        </p>
+
+        <a href="client_coach_page2.php?coach=Sofee">
+            <button>Choose Coach</button>
+        </a>
+    </div>
+</div>
+</body>
+</html>
---
+++ client_history.php
@@ -0,0 +1,97 @@
+<?php include 'headerClient.php'; ?>
+
+<div class="history-container">
+    <div class="stats">
+
+        <div class="card">
+            <h2>Weight Trend (Last 30 days)</h2>
+
+        <div class="chart">
+        Weight Trend Graph
+        </div>
+        </div>
+
+        <div class="card">
+            <h2>AVG Calorie Intake</h2>
+            <div class="chart">
+                Calorie Intake Chart
+            </div>
+        </div>
+
+        <div class="card">
+            <h2>Goal Consistency</h2>
+
+            <div class="circle">
+                25/31
+            </div>
+            <p>On-track Days (This Month)</p>
+            <br>
+            <p>Off-track Days (This Month)</p>
+        </div>
+    </div>
+
+    <div class="table-section">
+    <div class="table-title">
+        Detailed Log History
+    </div>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Date</th>
+                <th>Kcal In</th>
+                <th>Kcal Out</th>
+                <th>Steps</th>
+                <th>Primary Activity</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>15/04/2026</td>
+                <td>2000</td>
+                <td>1850</td>
+                <td>1500</td>
+                <td>Morning Walk</td>
+                <td>Completed</td>
+            </tr>
+
+            <tr>
+                <td>16/04/2026</td>
+                <td>1850</td>
+                <td>1700</td>
+                <td>1800</td>
+                <td>Running</td>
+                <td>Completed</td>
+            </tr>
+            <tr>
+                <td>17/04/2026</td>
+                <td>2100</td>
+                <td>1900</td>
+                <td>2000</td>
+                <td>Cycling</td>
+                <td>Completed</td>
+            </tr>
+            <tr>
+                <td>18/04/2026</td>
+                <td>1900</td>
+                <td>1700</td>
+                <td>1700</td>
+                <td>Gym Workout</td>
+                <td>Completed</td>
+            </tr>
+            <tr>
+                <td>19/04/2026</td>
+                <td>2000</td>
+                <td>1800</td>
+                <td>2200</td>
+                <td>Walking</td>
+                <td>Completed</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+</div>
+
+</body>
+</html>
