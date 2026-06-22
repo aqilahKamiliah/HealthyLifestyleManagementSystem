b9a38c4d7837720804f16ad179650b89141f5b26
aqilahKamiliah <aqilahkamiliah06@gmail.com>
2026-06-23 03:14:34 +0800

    Aqilah (#22)
    * add page log out for client and coach
    
    * add database connecting to admin

--- homeAdmin.php
+++ homeAdmin.php
@@ -9,30 +9,40 @@
 <body>
     <?php
     include "headerAdmin.php";
+    include 'connection.php';
 
-    $totalClient= "6";
-    $totalCoaches= "3";
-    $totalFood= "6";
+    // Function to get counts from your specific table names
+    function getCount($table, $conn) {
+    $sql = "SELECT COUNT(*) as total FROM $table";
+    $result = mysqli_query($conn, $sql);
+    $data = mysqli_fetch_assoc($result);
+    return $data['total'];
+    }
 
-    echo "<div class='dashboard'>";
-    echo "<div class='card'>";
-    echo "<h4> Total Clients </h4>";
-    echo "<p>".$totalClient."</p>";
-    echo "<a href = \"listClient.php\"> View Clients </a >";
-    echo "</div>";
-
-    echo "<div class='card'>";
-    echo "<h4> Total Coaches </h4>";
-    echo "<p>".$totalCoaches."</p>";
-    echo "<a href = \"listCoach.php\"> View Coaches </a >";
-    echo "</div>";
-
-    echo "<div class='card'>";
-    echo "<h4> Total Food </h4>";
-    echo "<p>".$totalFood."</p>";
-    echo "<a href = \"listFood.php\"> View Food </a >";
-    echo "</div>";
-    echo "</div>";
+// Fetching counts based on the table names in image_8e0987.png
+    $totalClient = getCount("client", $conn);
+    $totalCoaches = getCount("coach", $conn);
+    $totalFood = getCount("food", $conn);
     ?>
+
+    <div class='dashboard'>
+    <div class='card'>
+        <h4> Total Clients </h4>
+        <p><?php echo $totalClient; ?></p>
+        <a href="listClient.php"> View Clients </a>
+    </div>
+
+    <div class='card'>
+        <h4> Total Coaches </h4>
+        <p><?php echo $totalCoaches; ?></p>
+        <a href="listCoach.php"> View Coaches </a>
+    </div>
+
+    <div class='card'>
+        <h4> Total Food </h4>
+        <p><?php echo $totalFood; ?></p>
+        <a href="listFood.php"> View Food </a>
+    </div>
+    </div>
 </body>
 </html>
